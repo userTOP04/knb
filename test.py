ddd66e156b9a4dfa79051ebc21788b8620318bb6
userTOP04 <112961367+userTOP04@users.noreply.github.com>
2022-09-16 17:27:18 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -1,15 +1,28 @@
 import random
-user_turn = int(input("Введите число: 1 - камень, 2 -ножницы или 3 - бумага"))
-computer_turn = random.randint(1, 3)
 
-print("Игрок показал", user_turn)
-print("Компьютер показал", computer_turn)
+print("1 - камень, 2 - ножницы, 3 - бумага")
+
+# ход игрока
+user_turn = int(input("Введите число от 1 до 3"))
+if user_turn == 1:
+    print("Игрок показал камень")
+elif user_turn == 2:
+    print("Игрок показал ножницы")
+elif user_turn == 3:
+    print("Игрок показал бумага")
+
+# ход компа
+computer_turn = random.randint(1, 3)
+if computer_turn == 1:
+    print("Компьютер показал камень")
+elif computer_turn == 2:
+    print("Компьютер показал ножницы")
+elif computer_turn == 3:
+    print("Компьютер показал бумага")
 
 
 if user_turn == computer_turn:
     print("Ничья")
-
-
 else:
     if user_turn == 1 and computer_turn == 2:
         print ("Игрок победил")
