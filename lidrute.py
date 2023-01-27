cc3838a46c48e78dd9528c636913de552cf10a9e
userTOP04 <112961367+userTOP04@users.noreply.github.com>
2023-01-27 17:42:20 +0300

    Add files via upload

--- lidrute.py
+++ lidrute.py
@@ -1,36 +1,48 @@
 LIBRARY = []
 
 
-
+book_numders = 1
 
 def add_book():
 	print("Добовляем книшу")
 
-    title = input("Введите название книги:")
-    if not title:
-    	print("Ошибка не указано название")
-    	return
-    author = input("Введите имя Автора: ")
-    if not author:
-    	print("Ошибка не указано Автор")
-    	return
-    year = ("Введите год издания:" )
-    if not title:
-    	print("Ошибка не указан год издания")
-    	return
-    if year.isdigit():
-    	year = int(year)
-    else:
-    	print("Ошибка! Год должен быть цельным числом")
-    	return
+	title = input("Введите название книги:")
+	if not title:
+		print("Ошибка не указано название")
+		return
+	author = input("Введите имя Автора: ")
+	if not author:
+		print("Ошибка не указано Автор")
+		return
+	year = input("Введите год издания: ")
+	if not year:
+		print("Ошибка не указан год издания")
+		return
+		if year.isdigit():
+			year = int(year)
+		else:
+			print("Ошибка! Год должен быть цельным числом")
+			return
 
 
-    book = {
-    	"Название": title,
-    	"Автор": author,
-    	"Год": year
-    }
+	book = {
+		"Название": title,
+		"Автор": author,
+		"Год": year
+	}
 
 
-    LIBRARY.append(book)
-    print(f"Книга {title} {author} {year} успешно добавлена в библиотеку ")
+	LIBRARY.append(book, book_numders)
+	print(f"Книга {title} {author} {year} успешно добавлена в библиотеку ")
+	book_numders += 1
+
+
+def show_books():
+	for num, book in enumirate(LIBRARY, 1):
+		print("Номер на полке", num)
+		print("Название", book["название"])
+		print("Автор", book["автор"])
+		print("Год", book["год"])
+
+add_book()
+show_books()
